544333471a0329ba148c608628120ffa2cbeda3f
moneyan9 <m.tanimura1226@gmail.com>
2021-04-19 18:50:16 +0900

    initialize client

---
+++ client/src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import 'styles/globals.css'
+
+import type { NextPage } from 'next'
+import type { AppProps } from 'next/dist/next-server/lib/router/router'
+
+const MyApp: NextPage<AppProps> = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />
+}
+
+// eslint-disable-next-line import/no-default-export
+export default MyApp
---
+++ client/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next'
+import styles from 'styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return <div className={styles.container}>{/* ... */}</div>
+}
+
+// eslint-disable-next-line import/no-default-export
+export default Home
