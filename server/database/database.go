d188cfc9999871cfb070594561170b90183041cd
moneyan <m.tanimura1226@gmail.com>
2021-04-29 17:08:22 +0900

    refactor

--- server/database/database.go
+++ server/database/database.go
@@ -6,7 +6,7 @@ import (
 )
 
 func GetDatabase() *gorm.DB {
-	dsn := "user:password@tcp(localhost:3306)/mydb?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "user:password@tcp(db:3306)/mydb?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err.Error)
